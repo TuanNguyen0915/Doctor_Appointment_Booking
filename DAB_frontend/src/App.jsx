fa5d029d8690c842e0017fdb800528e44e0603ad
Tuan Nguyen <tuan.nat915@gmail.com>
2023-11-27 17:09:59 -0500

    initial git and custom tailwind

---
+++ DAB_frontend/src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div className='flex justify-center items-center'>
+      <h1 className='text-[30px] text-bold text-orange-600'>Hello world</h1>
+    </div>
+  )
+}
+
+export default App
